7fe910ecad877c4eb7506fff4b08dea84a44ff92
SwiftingDuster <tingwei1734@gmail.com>
2021-08-10 17:46:08 +0800

    Implement argparse and use naming convention

--- mp3tag.py
+++ mp3tag.py
@@ -1,144 +1,155 @@
+import argparse
 import os
 import re
-from sys import argv
 
-from hanziconv import HanziConv
+from mutagen import File
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import ID3NoHeaderError
-from mutagen import File
 
-argCount = len(argv)
-if argCount < 2:
-    print("Usage: Mp3Tag.py <filePath | dirPath> [format string] [index]")
-    os._exit(0)
+parser = argparse.ArgumentParser(
+    description="Extract mp3 data from filename and write it to the file's metadata.",
+    epilog="Usage: mp3tag.py <filePath | dirPath> -f [format string]",
+)
 
-targetPath = os.getcwd() if argv[1] == '.' else argv[1]
+parser.add_argument("path", help="Folder containing mp3 files, or a single mp3 file.")
+parser.add_argument(
+    "-f",
+    "--format",
+    default="%artist% - %title%",
+    help="Filename format used to convert.",
+)
+args = parser.parse_args()
 
-convertFormatString = '%artist% - %title%'
-if argCount > 2:
-	convertFormatString = convertFormatString if argv[2] == '.' else argv[2].lower()
+target_path = os.getcwd() if args.path == "." else args.path
+format_string = args.format.lower()
 
-if not os.path.exists(targetPath):  # The path specified doesn't exist
+# Path and format string validation
+if not os.path.exists(target_path):
+    # The path specified doesn't exist
     print("Error: Given path does not exist.")
-    os._exit(0)
-# Format string has odd number of % and therefore invalid
-elif convertFormatString.count("%") % 2 != 0:
+    exit(1)
+elif "%" not in format_string or format_string.count("%") % 2 != 0:
+    # Format string has odd number of % and therefore invalid
     print("Error: Format string is not valid.")
-    os._exit(0)
+    exit(1)
 
-# Get all formatting placeholders E.g. (%artist%, %title%, %genre%, %album%)
-formatterList = re.findall(r"(\%.+?\%)", convertFormatString)
+# Full list of metadata keys from EasyID3.valid_keys.keys()
+# 'album', 'bpm', 'compilation', 'composer', 'copyright', 'encodedby', 'lyricist', 'length', 'media',
+# 'mood', 'title', 'version', 'artist', 'albumartist', 'conductor', 'arranger', 'discnumber',
+# 'organization', 'tracknumber', 'author', 'albumartistsort', 'albumsort', 'composersort', 'artistsort',
+# 'titlesort', 'isrc', 'discsubtitle', 'language', 'genre', 'date', 'originaldate', 'performer:*',
+# 'musicbrainz_trackid', 'website', 'replaygain_*_gain', 'replaygain_*_peak', 'musicbrainz_artistid',
+# 'musicbrainz_albumid', 'musicbrainz_albumartistid', 'musicbrainz_trmid', 'musicip_puid',
+# 'musicip_fingerprint', 'musicbrainz_albumstatus', 'musicbrainz_albumtype', 'releasecountry',
+# 'musicbrainz_discid', 'asin', 'performer', 'barcode', 'catalognumber', 'musicbrainz_releasetrackid',
+# 'musicbrainz_releasegroupid', 'musicbrainz_workid', 'acoustid_fingerprint', 'acoustid_id'
+
+# Get all formatting placeholders in between % signs. E.g. %artist%, %title%, %genre%, %album%
+formatters = re.findall(r"(\%.+?\%)", format_string)
+# Implemented metadata keys
+ACCEPTED_FORMATTERS = ["title", "artist", "album", "genre"]
 
 # Check validity of formatting placeholders
-for formatter in formatterList:
-    if formatter[1:-1] not in EasyID3.valid_keys.keys():
+for formatter in formatters:
+    if formatter[1:-1] not in ACCEPTED_FORMATTERS:
         print(f"ERROR: Formatter '{formatter}' is not valid.")
-        os._exit(0)
+        exit(1)
 
-# Replace all formatters with a regex string and compile it. # %artist% - %title% => \b(.*)\b - \b(.*)\b
-convertFormatRegexString = convertFormatString
-for formatter in formatterList:
-    convertFormatRegexString = convertFormatRegexString.replace(formatter, r"\b(.*)\b") 
-convertFormatRegex = re.compile(convertFormatRegexString)
 
-totalCount = 0
-processedCount = 0
-skippedFilesDict = {}
+# Replace all formatters with a regex string and compile it. E.g. %artist% - %title% => \b(.*)\b - \b(.*)\b
+formatter_regex_str = format_string
+for formatter in formatters:
+    formatter_regex_str = formatter_regex_str.replace(formatter, r"\b(.*)\b")
+convertFormatRegex = re.compile(formatter_regex_str)
 
-def skip(filePath, reason):
-    global processedCount
-    skippedFilesDict[filePath] = reason
-    print(reason)
-    return
 
-def convert(filePath):
+# Setup counters
+total_count = 0
+processed_count = 0
+error_reason_dict = {}
+
+
+def convert(file_path):
+    global processed_count
+
     # Get the file name without extension
-    fileNameWithExt = os.path.basename(filePath)
+    filename_with_ext = os.path.basename(file_path)
 
-    global processedCount, skippedFilesDict
-    
-    nameExtTuple = os.path.splitext(fileNameWithExt)
-    if (nameExtTuple[1] != ".mp3"): # If not a MP3 file
-        reason = f"Skipped non mp3 file: {filePath}\n"
-        skip(filePath, reason)
-        return
+    try:
+        from hanziconv import HanziConv
+    except ImportError:
+        pass
+    else:
+        # Convert traditional chinese to simplified chinese
+        file_name = HanziConv.toSimplified(os.path.splitext(filename_with_ext)[0])
 
-    # Convert traditional chinese to simplified chinese
-    fileName = HanziConv.toSimplified(nameExtTuple[0])
-
-    # If file name was modified, reflect the change in the real file.
-    newFilePath = os.path.join(os.path.dirname(filePath), f"{fileName}.mp3")
-    if filePath != newFilePath:
-        os.rename(filePath, newFilePath)
-        filePath = newFilePath
+        # If file name was modified, write the change to the file.
+        newFilePath = os.path.join(os.path.dirname(file_path), f"{file_name}.mp3")
+        if file_path != newFilePath:
+            os.rename(file_path, newFilePath)
+            file_path = newFilePath
 
     # Capture groups from file name using regex created from provided format string.
-    matches = convertFormatRegex.match(fileName)
-    
-    if (matches == None):
-        reason = f"Skipped unmatched file: {filePath}\n"
-        skip(filePath, reason)
+    matches = convertFormatRegex.match(file_name)
+
+    if matches == None:
+        error_reason_dict[
+            filename_with_ext
+        ] = "Check that filename matches with format pattern."
+        processed_count += 1
         return
 
     try:
-        mp3 = EasyID3(filePath)
-        mp3.delete() # Delete existing ID3 tag
+        mp3 = EasyID3(file_path)
+        mp3.delete()  # Delete existing ID3 tag
     except ID3NoHeaderError:
-        mp3 = File(filePath, easy=True)
-        mp3.add_tags() # Add new ID3 tag
-    
-    processedCount += 1
-    print(f"Processing file {processedCount} of {totalCount}: {filePath}")
+        mp3 = File(file_path, easy=True)
+        mp3.add_tags()  # Add new ID3 tag
+
+    processed_count += 1
+
+    print(f"Processed file {processed_count} of {total_count}: {filename_with_ext}")
 
     # Apply the values
     for index, value in enumerate(matches.groups()):
-        formatter = formatterList[index]
-        if formatter == r"%title%":
+        formatter = formatters[index]
+        if formatter == "%title%":
             mp3["title"] = value
-        elif formatter == r"%artist%":
-            mp3["artist"]= value
-        elif formatter == r"%album%":
+        elif formatter == "%artist%":
+            mp3["artist"] = value
+        elif formatter == "%album%":
             mp3["album"] = value
-        elif formatter == r"%genre%":
+        elif formatter == "%genre%":
             mp3["genre"] = value
-        elif formatter == r"%comment%":
-            mp3["comment"] = value
-        elif formatter in EasyID3.valid_keys.keys():
-            print(f"ERROR: Formatter '{formatter}' is available but not implemented.")
+        elif formatter in ACCEPTED_FORMATTERS:
+            print(f"ERROR: Formatter '{formatter}' not implemented.")
             return
-        print(f"Formatter Interpret: {formatter} ==> {value}")
-    print()
 
     # Save
     mp3.save()
 
+
 print("========================================")
-print(f"Target path: {targetPath}")
-print(f"Formatter: {convertFormatString}")
+print(f"Target path: {target_path}")
+print(f"Formatter: {format_string}")
 print()
 
-if os.path.isdir(targetPath):
-    fileList = [os.path.join(targetPath, fileName) for fileName in os.listdir(targetPath)]
-    
-    startIndex = 0
-    if argCount > 3:
-        if argv[3].isnumeric():
-            indexRaw = int(argv[3]) - 1
-            startIndex =  indexRaw if indexRaw >= 0 and indexRaw < len(fileList) else 0 
-            print(f"Starting from file number: {argv[3]}")
-        else:
-            print(f"ERROR: Start index is not a number: {argv[3]}")
-            os._exit(0)
-
-    totalCount = len(fileList[startIndex:])
-
-    for file in fileList[startIndex:]:
+if os.path.isdir(target_path):
+    fileList = [
+        os.path.join(target_path, file_name)
+        for file_name in os.listdir(target_path)
+        if file_name.endswith(".mp3")
+    ]
+    total_count = len(fileList)
+    for file in fileList:
         convert(file)
 else:
-    totalCount = 1
-    convert(targetPath)
+    total_count = 1
+    convert(target_path)
 
-print(f"Job done for {totalCount} files. Processed: {processedCount}, Skipped: {len(skippedFilesDict)}")
-for file in skippedFilesDict.keys():
-    print(f"{skippedFilesDict[file]} {file}")
+print(
+    f"Job done for {total_count} files. Processed: {processed_count}, Errored: {len(error_reason_dict)}"
+)
+for file in error_reason_dict.keys():
+    print(f"{error_reason_dict[file]} {file}")
 print("========================================")
